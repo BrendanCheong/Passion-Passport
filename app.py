f06676289314b9277b6914a2b6cf08577dd28c1b
harrychangjr <71932325+harrychangjr@users.noreply.github.com>
2023-05-28 13:37:15 +0800

    multipage demo

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import streamlit as st
+# Set page title
+
+st.set_page_config(page_title="Travel App", page_icon = "✈️", layout = "centered", initial_sidebar_state = "auto")
+
+st.header("Travel App - LifeHack")
+
 
 st.write("hello world")
